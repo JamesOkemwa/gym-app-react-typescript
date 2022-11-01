0cb0df72bcd2db5930dc1a5b1a1cb0cd98818120
lejames <okejames55@gmail.com>
2022-11-01 17:10:40 +0300

    Detail component

---
+++ src/components/Detail.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IExercise } from '../utils/fetchData'
+
+
+type DetailProps = {
+    exerciseDetail: IExercise
+}
+
+const Detail = ({ exerciseDetail }: DetailProps) => {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
